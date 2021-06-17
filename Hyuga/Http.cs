4e846f7a12ebae80d2c60ba0b0d5f27976ffbc03
Kenyh <71294924+xKenyh@users.noreply.github.com>
2021-06-16 22:36:42 -0300

    Add files via upload

---
+++ Hyuga/Http.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Net;
+
+namespace Hyuga
+{
+    internal class Http
+    {
+        public Http()
+        {
+        }
+
+        public static byte[] Post(string uri, NameValueCollection pairs)
+        {
+            byte[] numArray;
+            using (WebClient webClient = new WebClient())
+            {
+                numArray = webClient.UploadValues(uri, pairs);
+            }
+            return numArray;
+        }
+    }
+}
